4a1d381d4c2e42b227077d0c5d18b2196c8b5cfa
ErastKoliasa <erastyk28@gmail.com>
2023-01-31 18:22:00 +0200

    added React.lazy and React.Suspense to the application (Profile page, Message page, Login page, Users page)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import { Routes, Route } from "react-router-dom";
-import MessagesContainer from './components/Messages/MessagesContainer';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
-import Login from './components/Login/Login';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { connect } from 'react-redux';
 import { initializeAppThunkCreator } from './redux/appReducer';
 import PreLoader from './components/common/PreLoader/PreLoader';
+
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const MessagesContainer = React.lazy(() => import('./components/Messages/MessagesContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const Login = React.lazy(() => import('./components/Login/Login'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -25,14 +26,16 @@ class App extends React.Component {
         <HeaderContainer />
         <NavBar />
         <div className='content'>
-          <Routes>
-            <Route path="/messages" element={<MessagesContainer />} />
-            <Route path="/profile" element={<ProfileContainer />}>
-              <Route path=":userId" element={<ProfileContainer />} />
-            </Route>
-            <Route path="/users" element={<UsersContainer />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
+          <Suspense fallback={<PreLoader />}>
+            <Routes>
+              <Route path="/messages" element={<MessagesContainer />} />
+              <Route path="/profile" element={<ProfileContainer />}>
+                <Route path=":userId" element={<ProfileContainer />} />
+              </Route>
+              <Route path="/users" element={<UsersContainer />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Suspense>
         </div>
         <Footer />
       </div>
