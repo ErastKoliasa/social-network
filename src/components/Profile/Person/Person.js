c8ef1088a2286976b5e101427bd12f436037092e
ErastKoliasa <erastyk28@gmail.com>
2023-02-06 23:03:49 +0200

    added the implementation of displaying user information on the "Profile" page

--- src/components/Profile/Person/Person.js
+++ src/components/Profile/Person/Person.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PreLoader from '../../common/PreLoader/PreLoader';
 import styles from './Person.module.css'
 import photo from '../../../assets/img/avatar.png'
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import ProfileDataForm from './ProfileDataForm';
 
 const Person = (props) => {
 
+    let [editMode, setEditMode] = useState(false);
+
     const onMainPhotoSelected = (e) => {
-        if(e.target.files.length){
+        if (e.target.files.length) {
             props.savePhoto(e.target.files[0]);
         }
     }
@@ -27,16 +30,33 @@ const Person = (props) => {
         <div className={styles.person}>
             <div className={styles.containerPersonPhoto}>
                 <img className={styles.personPhoto} src={avatarPhoto()}></img>
-                {props.isOwner && <input className={styles.btnChangePhoto} type={"file"} onChange={onMainPhotoSelected}/>}
+                {props.isOwner && <input className={styles.btnChangePhoto} type={"file"} onChange={onMainPhotoSelected} />}
             </div>
-           
-            <div className={styles.containerPersonInfo}>
-                <p className={styles.fullname}>{props.profile.fullName}</p>
-                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
-                <p className={styles.aboutMe}>{props.profile.aboutMe}</p>
-            </div>
+            {editMode ? <ProfileDataForm profile={props.profile} saveProfile={props.saveProfile}/>
+                : <ProfileData profile={props.profile} isOwner={props.isOwner} goToEditMode={() => { setEditMode(true) }} />}
         </div>
     )
+}
+
+const ProfileData = (props) => {
+    return <div className={styles.containerPersonInfo}>
+        <p className={styles.fullname}>{props.profile.fullName}</p>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
+        <p>Looking for a job: {props.profile.lookingForAJob ? "yes" : "no"}</p>
+        {props.profile.lookingForAJob &&
+            <p>My profesional skills: {props.profile.lookingForAJobDescription}</p>
+        }
+        <p className={styles.aboutMe}>About me: {props.profile.aboutMe}</p>
+        <p>Contacts: {Object.keys(props.profile.contacts).map(key => {
+            return <Contacts key={key} contactTitle={key} contactValue={props.profile.contacts[key]} />
+        })}</p>
+        {props.isOwner && <button onClick={props.goToEditMode}>Edit</button>}
+    </div>
+
+}
+
+const Contacts = (props) => {
+    return <p>{props.contactTitle}: {props.contactValue}</p>
 }
 
 export default Person;
