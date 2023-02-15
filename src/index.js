7bd4dbad140ec8274b299e7dafc25809204e56d3
ErastKoliasa <erastyk28@gmail.com>
2023-02-15 16:47:00 +0200

    The application has been deployed on gitHub pages

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/reduxStore';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
 
